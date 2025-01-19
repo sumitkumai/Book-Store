1ad909ac8c43c235225fdfeee3b96af2b7fbc44f
Sumit Kumai <kumaisumit688@gmail.com>
2025-01-19 19:08:13 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,10 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import cors from 'cors';
 
-import bookroute from './routes/book.route.js'
-import userroute from './routes/user.route.js'
+import bookroute from './routes/book.route.js';
+import userroute from './routes/user.route.js';
 
 const app = express();
 dotenv.config();
@@ -12,36 +12,34 @@ dotenv.config();
 app.use(express.json());
 
 const allowedOrigins = [
-    'http://localhost:5173', // Local frontend
-    'https://book-store-seven-tau-70.vercel.app/' // Production frontend (Vercel)
-  ];
-  
-  app.use(cors({
+    'http://localhost:5173',
+    'https://book-store-seven-tau-70.vercel.app'
+];
+
+app.use(cors({
     origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true); // Allow request from allowed origins or no origin (e.g., Postman)
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-  }));
+}));
 
 const PORT = process.env.port || 4000;
 const MONGO_URI = process.env.mongo_URI;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
-
-mongoose.connect(MONGO_URI,{dbName:"BookShelf"})
-.then(async() => {
-    console.log('Connected to MongoDB');
-})
-.catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
 });
 
+mongoose.connect(MONGO_URI, { dbName: "BookShelf" })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err.message);
+    });
 
-// defining route
-app.use('/api',bookroute)
-app.use('/api',userroute)
+app.use('/api', bookroute);
+app.use('/api', userroute);
