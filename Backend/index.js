f4ea46d229d1e34388ade5b9f20a93e45a9846bc
Sumit Kumai <kumaisumit688@gmail.com>
2025-01-19 18:59:27 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -26,8 +26,8 @@ const allowedOrigins = [
     }
   }));
 
-const PORT = process.env.PORT || 4000;
-const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.port || 4000;
+const MONGO_URI = process.env.mongo_URI;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
